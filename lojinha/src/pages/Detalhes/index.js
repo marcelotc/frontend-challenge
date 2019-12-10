170c663d9903257a35965e54fdabb4c562b94bb5
= <=>
2019-12-10 14:35:19 -0300

    Estilizacao da pagina detalhes

--- lojinha/src/pages/Detalhes/index.js
+++ lojinha/src/pages/Detalhes/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { Image, Button } from 'react-bootstrap';
 
 import './styles.css';
+
+import ImagemQuebrada from '../../assets/imagem.jpg'
 
 export default class Loja extends Component {
   state = {
@@ -22,8 +29,22 @@ export default class Loja extends Component {
     const { loja } = this.state;
 
     return (
-      <div>
-        <h1>{loja.name}</h1>
+      <div className="corpo-detalhes">
+        <Link to={`/`} className="button"><FontAwesomeIcon icon={faHome} size="2x" /></Link>
+        <div className="text-center">
+          <h1>{loja.name}</h1>
+          <Image variant="top" onError={(e) => e.target.src = ImagemQuebrada} height="80px" src={loja.image_blob} alt="imagem" thumbnail />
+          <br></br><br></br>
+          <h5>Categorias:</h5>
+         
+          <div className="nota">
+            <h5>Nota: {loja.rating}</h5>
+          </div>
+          <br></br>
+          <a href={loja.url} >
+            <Button variant="primary">Ir para o site da loja</Button>
+          </a>
+        </div>
       </div>
     );
   }
